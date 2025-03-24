36c8f16833c254076125004d2c81e7d78370d47b
Acer <Acer@DESKTOP-660TC1S.mshome.net>
2025-03-24 16:06:43 +0530

    Push

---
+++ Sample_Project/src/main/java/ObjectRepo/Example_ExTest.java
@@ -0,0 +1,11 @@
+package ObjectRepo;
+
+import org.testng.annotations.Test;
+
+public class Example_ExTest {
+	@Test
+	public void createPomClassTest() {
+		System.out.println("Hiii");
+	}
+
+}
---
+++ Sample_Project/src/test/java/SampleProject_Ex/Example1.java
@@ -0,0 +1,13 @@
+package SampleProject_Ex;
+
+import org.testng.annotations.Test;
+
+public class Example1 {
+		@Test
+		public void createPomClassTest() {
+			System.out.println("Hiii");
+		}
+
+	}
+
+
