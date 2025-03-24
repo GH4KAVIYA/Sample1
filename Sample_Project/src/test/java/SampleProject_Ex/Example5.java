332464f4b45fbe184618ab375674f415b742acd8
Acer <Acer@DESKTOP-660TC1S.mshome.net>
2025-03-24 17:40:33 +0530

    Added another class

---
+++ Sample_Project/src/test/java/SampleProject_Ex/Example5.java
@@ -0,0 +1,16 @@
+package SampleProject_Ex;
+
+import org.testng.annotations.Test;
+
+public class Example5 {
+	@Test
+	public void createPomClassTest() {
+		System.out.println("Hiii");
+	}
+	
+	@Test
+	public void createExampleClassTest() {
+		System.out.println("Hello");
+	}
+
+}
