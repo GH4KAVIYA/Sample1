b1a5589f5a8734d9a65f9111b68cc98f9012d102
Acer <Acer@DESKTOP-660TC1S.mshome.net>
2025-04-02 17:16:38 +0530

    first push

---
+++ Sample_Project/src/test/java/SampleProject_pack1/OrgTest.java
@@ -0,0 +1,16 @@
+package SampleProject_pack1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("Excute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("Excute modifyOrgTest");
+	}
+
+}
